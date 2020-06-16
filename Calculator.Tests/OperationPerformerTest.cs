bfee8d0fcb43ea58c6e58ddb5e57a178987eae64
Natalya Posokhova <NPosokhova@RUCHE-1TON>
2020-06-17 01:31:17 +0500

    Refactoring. Unit test for OprationPerformer was added

---
+++ Calculator.Tests/OperationPerformerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorProject;
+
+namespace CalcTests
+{
+    [TestClass]
+    public class OperationPerformerTest
+    {
+        [TestMethod]
+        public void CalcBracketLessExpressionTestMethod()
+        {
+            // arrange
+            var expressionBracketsLess = "8/4+6-5-4";
+
+            var performer = new OperationPerformer();
+
+            // act
+            var actualResult = performer.CalcBracketLessExpression(expressionBracketsLess);
+
+            // assert
+            Assert.AreEqual("-1", actualResult);
+        }
+    }
+}
