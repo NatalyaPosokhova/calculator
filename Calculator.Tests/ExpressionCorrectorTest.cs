ebe5e2bc541bfb5b06aba44a83d2e84a14eae256
Natalya Posokhova <NPosokhova@RUCHE-1TON>
2020-06-17 17:30:14 +0500

    Refactoring. Unit test for ExpressionCorrector was added

---
+++ Calculator.Tests/ExpressionCorrectorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorProject;
+
+namespace CalcTests
+{
+    [TestClass]
+    public class ExpressionCorrectorTest
+    {
+        [TestMethod]
+        public void CorrectTestMethod()
+        {
+            // arrange
+            var expression = "4 + 2 - 8.5 = ";
+            var corrector = new ExpressionCorrector();
+
+            // act
+            var actualResult = corrector.Correct(expression);
+
+            // assert
+            Assert.AreEqual("4+2-8,5", actualResult);
+        }
+    }
+}
