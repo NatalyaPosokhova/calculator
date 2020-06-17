a09dbfbafcf472398a82a4ca7067e7e78fa0ec95
Natalya Posokhova <NPosokhova@RUCHE-1TON>
2020-06-17 17:07:50 +0500

    Refactoring. Minor chages. Unit tests for ExpressionValidator were transferred into another file

---
+++ Calculator.Tests/ExpressionValidatorTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorProject;
+
+namespace CalcTests
+{
+    [TestClass]
+    public class ExpressionValidatorTest
+    {
+        [TestMethod]
+        public void UnitTestDifferentBracketsQuantityException()
+        {
+            // arrange
+            var expression = "(7+8*(9-5)))";
+            var validator = new ExpressionValidator();
+
+            // act
+            var actualResult = validator.Validate(expression);
+
+            // assert
+            Assert.AreEqual(actualResult, validator.ErrorBracketsQuantity);
+        }
+
+        [TestMethod]
+        public void UnitTestExceededSymbolsException()
+        {
+            // arrange
+            var expression = "(7+8*&(9-5))?";
+            var validator = new ExpressionValidator();
+
+            // act
+            var actualResult = validator.Validate(expression);
+
+            // assert
+            Assert.AreEqual(actualResult, validator.ErrorExceededSymbols);
+        }
+
+        [TestMethod]
+        public void UnitTestEmptyExpressionException()
+        {
+            // arrange
+            var expression = "";
+            var validator = new ExpressionValidator();
+
+            // act
+            var actualResult = validator.Validate(expression);
+
+            // assert
+            Assert.AreEqual(actualResult, validator.ErrorEmpty);
+        }
+
+        [TestMethod]
+        public void UnitTestOverflowException()
+        {
+            // arrange
+            Random rand = new Random();
+            string[] operators = { "+", "-", "*", "/" };
+
+            var expression = "";
+
+            for (int i = 0; i < 501; i++)
+            {
+                string randomOperator = operators[rand.Next(operators.Length)];
+                var randomNumber = rand.Next(-1000, 1000);
+
+                expression += $"{randomNumber.ToString()}{randomOperator}-1";
+            }
+
+            var validator = new ExpressionValidator();
+
+            // act
+            var actualResult = validator.Validate(expression);
+
+            // assert
+            Assert.AreEqual(actualResult, validator.ErrorTooLong);
+        }
+    }
+}
